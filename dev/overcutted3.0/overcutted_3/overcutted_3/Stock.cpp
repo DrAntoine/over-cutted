82f276bb548b503b3be9452e4175bdc8f79a5c0e
etteloc <benjamin.colette.bc@gmail.com>
2021-05-09 16:01:39 +0200

    sprite
    mise a jour des pos des outils

--- dev/overcutted3.0/overcutted_3/overcutted_3/Stock.cpp
+++ dev/overcutted3.0/overcutted_3/overcutted_3/Stock.cpp
@@ -1,1 +1,17 @@
 #include "Stock.h"
+
+Stock::Stock(sf::Vector2u position, DeplacableType typeAlimentAFournir)
+{
+	if (typeAlimentAFournir == DeplacableType::assiette)
+	{
+		m_sprite = m_textureManager->getTexture(TextureType::Outils, sf::Vector2u(1, 2));
+	}
+	if (typeAlimentAFournir == DeplacableType::poisson)
+	{
+		m_sprite = m_textureManager->getTexture(TextureType::Outils, sf::Vector2u(1, 1));
+	}
+	if (typeAlimentAFournir == DeplacableType::crevette)
+	{
+		m_sprite = m_textureManager->getTexture(TextureType::Outils, sf::Vector2u(0, 3));
+	}
+}
--- dev/overcutted3.0/overcutted_3/overcutted_3/Tuile.cpp
+++ dev/overcutted3.0/overcutted_3/overcutted_3/Tuile.cpp
@@ -1,1 +1,5 @@
 #include "Tuile.h"
+Tuile::Tuile()
+{
+	m_sprite = m_textureManager->getTexture(TextureType::Outils, sf::Vector2u(0, 0));
+}
