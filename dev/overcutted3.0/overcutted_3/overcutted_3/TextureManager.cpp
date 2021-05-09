75a6d0fe46c793459fd155350d156cb90b535535
Chaoui <antoine.druart@me.com>
2021-05-09 11:26:33 +0200

    Modification de l'accès aux textures dans TextureManager.cpp
    remplacement de positionInFile.x+50 par positionInFile.x + m_imageSize.x;

--- dev/overcutted3.0/overcutted_3/overcutted_3/TextureManager.cpp
+++ dev/overcutted3.0/overcutted_3/overcutted_3/TextureManager.cpp
@@ -5,6 +5,7 @@ TextureManager::TextureManager(std::string PersoPath, std::string OutilsPath, st
 	m_textAliments.loadFromFile(AlimentPath);
 	m_textOutils.loadFromFile(OutilsPath);
 	m_textPerso.loadFromFile(PersoPath);
+	m_imageSize = (50, 50);
 }
 
 void TextureManager::getTexture(TextureType m_type, sf::Vector2u positionInFile)
@@ -12,19 +13,19 @@ void TextureManager::getTexture(TextureType m_type, sf::Vector2u positionInFile)
 	if (m_type == TextureType::Aliments)
 	{
 		m_sprite.setTexture(m_textAliments);
-		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + 50, positionInFile.y + 50));
+		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + m_imageSize.x, positionInFile.y + m_imageSize.y));
 
 	}
 	if (m_type == TextureType::Outils)
 	{
 		m_sprite.setTexture(m_textOutils);
-		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + 50, positionInFile.y + 50));
+		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + m_imageSize.x, positionInFile.y + m_imageSize.y));
 
 	}
 	if (m_type == TextureType::Personnage)
 	{
 		m_sprite.setTexture(m_textPerso);
-		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + 50, positionInFile.y + 50));
+		m_sprite.setTextureRect(sf::IntRect(positionInFile.x, positionInFile.y, positionInFile.x + m_imageSize.x, positionInFile.y + m_imageSize.y));
 
 	}
 }
