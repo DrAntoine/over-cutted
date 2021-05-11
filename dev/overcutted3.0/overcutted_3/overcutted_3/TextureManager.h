c8cb16b236ffb8cedba9c7a5125624cca825a1ba
etteloc <benjamin.colette.bc@gmail.com>
2021-05-11 16:00:15 +0200

    renouveau
    retout en arrière

--- dev/overcutted3.0/overcutted_3/overcutted_3/TextureManager.h
+++ dev/overcutted3.0/overcutted_3/overcutted_3/TextureManager.h
@@ -14,7 +14,7 @@ class TextureManager
 {
 public:
 	TextureManager(/*const std::string PersoPath, const std::string OutilsPath, const std::string AlimentsPath*/);
-	sf::Sprite getTexture(TextureType m_type, sf::Vector2u positionInFile);
+	sf::Sprite getTexture(TextureType m_type,sf::Vector2u positionInFile);
 
 private:
 	sf::Texture m_textPerso;
